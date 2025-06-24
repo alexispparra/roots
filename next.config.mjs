9f0edd4bd51235335beaa1adadfdce18c15973f4
roots. Developers <roots.developers.fp@gmail.com>
2025-06-24 14:31:02 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,4 @@
-import type {NextConfig} from 'next';
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-    ],
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
