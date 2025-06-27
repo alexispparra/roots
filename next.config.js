1224124a8f0320afa2b2338e1476c10a3ca1bc14
roots. Developers <roots.developers.fp@gmail.com>
2025-06-27 22:15:06 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.js
+++ next.config.js
@@ -9,13 +9,6 @@ const nextConfig = {
     ],
   },
   reactStrictMode: true,
-  webpack: (config) => {
-    config.watchOptions = {
-      poll: 1000,
-      aggregateTimeout: 300,
-    };
-    return config;
-  },
 };
 
 module.exports = nextConfig;
